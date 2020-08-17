fd9cee411646a7d37326f8946a0795a3d549ccf4
Minh Vuong <minhvuong@live.com>
2020-08-17 13:48:03 +1000

    build

---
+++ build/precache-manifest.6fa5f8e3eb76762d8269ce7a27da11c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d70baabc0993e60c726e28ec60c2724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f09c5ab1355e529bc6ce",
+    "url": "/static/css/main.84de2ae0.chunk.css"
+  },
+  {
+    "revision": "0da13b3ee37345b673b8",
+    "url": "/static/js/2.62950324.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.62950324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09c5ab1355e529bc6ce",
+    "url": "/static/js/main.0f95dfce.chunk.js"
+  },
+  {
+    "revision": "4acb8efb8e3fca718544",
+    "url": "/static/js/runtime-main.b2977abe.js"
+  }
+]);
